6e526990c0e9328ea2aebba1394c36cf831fa35d
Arturo Cabrera <cabrera_a90>
2022-04-04 14:24:54 -0500

    pink turtle

--- turtle_square.py
+++ turtle_square.py
@@ -2,6 +2,9 @@ import turtle
 
 window = turtle.Screen()
 tortuga = turtle.Turtle()
+tortuga.shape("turtle")
+tortuga.pensize(3)
+tortuga.pencolor("deeppink")
 
 for i in range(4):
     tortuga.forward(100)
