c051d15ff16ebcce5544c0e0096ebbfb4b83933f
Arturo Cabrera <cabrera_a90>
2022-04-05 18:16:22 -0500

    implementation of Update function

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ def list_clients():
     print(clients)
 
 
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients=clients.replace(client_name, updated_client_name)
+        print('client '+ client_name+' was replaced for '+updated_client_name)
+    else:
+        print('client is not in clients list')
+
+
 
 def _add_comma():
     global clients
@@ -25,18 +35,26 @@ def _print_welcome():
     print('*'*50)
     print('what would you like to do today?')
     print('[C]reate client')
+    print('[U]pdate client')
     print('[D]elete client')
 
 
+def _get_client_name():
+    return input('What is the client name? >')
 
 if __name__ == '__main__':
     _print_welcome()
 
-    command = input()
+    command = input().upper()
 
     if command == 'C':
-        client_name = input('what is the client name? ')
+        client_name = _get_client_name()
         create_client(client_name)
+        list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input( 'what is the updated client name > ')
+        update_client(client_name,updated_client_name)
         list_clients()
     elif command=='D':
         pass
---
+++ playing.py
@@ -0,0 +1,13 @@
+art="Arturo"
+#print(a)
+
+def my_function():
+    """Este es un texto de ayuda de como utilizar esta función  """
+    pass
+
+print(id(art))
+art+=" Javier"
+print(id(art))
+art=art.upper()
+print(id(art))
+print(id("Arturo"))
