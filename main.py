5f1cd7da5ebf1e8a0101b67236b6d26c2941e8de
Arturo Cabrera <cabrera_a90>
2022-04-07 19:52:42 -0500

    added loop, and functions: Quit, list, search

--- main.py
+++ main.py
@@ -32,6 +32,16 @@ def delete_client(client_name):
         print('Client is not in clients list')
 
 
+def search_client(client_name):
+    list_clients = clients.split(',')
+    for client in list_clients:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+    return False
+
 def _add_comma():
     global clients
     clients += ','
@@ -42,30 +52,53 @@ def _print_welcome():
     print('*'*50)
     print('what would you like to do today?')
     print('[C]reate client')
+    print('[L]ist clients')
     print('[U]pdate client')
     print('[D]elete client')
+    print('[S]earch client')
+    print('*'*50)
+    print('[Q]uit program')
 
 
 def _get_client_name():
     return input('What is the client name? >')
 
 if __name__ == '__main__':
-    _print_welcome()
 
-    command = input().upper()
+    _quit = False
 
-    if command == 'C':
-        client_name = _get_client_name()
-        create_client(client_name)
-        list_clients()
-    elif command == 'U':
-        client_name = _get_client_name()
-        updated_client_name = input( 'what is the updated client name > ')
-        update_client(client_name,updated_client_name)
-        list_clients()
-    elif command=='D':
-        client_name = _get_client_name()
-        delete_client(client_name)
-        list_clients()
-    else:
-        print("invalid conmmand")
+    while not _quit:
+        _print_welcome()
+
+        command = input().upper()
+
+        if command == 'C':
+            client_name = _get_client_name()
+            create_client(client_name)
+            list_clients()
+            continue
+        elif command == 'L':
+            list_clients()
+            continue
+        elif command == 'U':
+            client_name = _get_client_name()
+            updated_client_name = input( 'what is the updated client name > ')
+            update_client(client_name,updated_client_name)
+            list_clients()
+            continue
+        elif command=='D':
+            client_name = _get_client_name()
+            delete_client(client_name)
+            list_clients()
+        elif command == 'S':
+            client_name = _get_client_name()
+            found = search_client(client_name)
+            if found:
+                print('the client is in the client\'s list')
+            else:
+                print('client {} is not in our client\'s list'.format(client_name))
+            continue
+        elif command == 'Q':
+            _quit = True
+        else:
+            print("invalid conmmand")
